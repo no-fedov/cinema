8a08ee19cc90aae6c484b4f21a5050c931095317
no_fedov <nefedov.evgeniy.d@yandex.ru>
2025-01-28 16:22:34 +0300

    feat: добавлены сущности

---
+++ src/main/java/javaacademy/com/cinema/entity/Place.java
@@ -0,0 +1,13 @@
+package javaacademy.com.cinema.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Place {
+    private String id;
+    private String name;
+}
