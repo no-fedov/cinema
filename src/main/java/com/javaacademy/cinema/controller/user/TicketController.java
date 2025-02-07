9a7ee4d5db3379be2e385459f828e6417514f551
no_fedov <nefedov.evgeniy.d@yandex.ru>
2025-02-07 03:41:23 +0300

    stage 5 (не добавлены обработки ошибок)

--- src/main/java/com/javaacademy/cinema/controller/user/TicketController.java
+++ src/main/java/com/javaacademy/cinema/controller/user/TicketController.java
@@ -5,6 +5,7 @@ import com.javaacademy.cinema.dto.TicketDto;
 import com.javaacademy.cinema.service.user.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class TicketController {
     private final TicketService ticketService;
 
     @PostMapping("/booking")
-    public TicketDto buyTicket(TicketBookingDto dto) {
+    public TicketDto buyTicket(@RequestBody TicketBookingDto dto) {
         TicketDto ticketDto = ticketService.buyTicket(dto);
         return ticketDto;
     }
